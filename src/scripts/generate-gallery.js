b46a3057a067fcc8c8381e5a62d24b8bb4a3730b
Roberto Almeida <almeida.robertop@hotmail.com>
2026-03-25 22:20:55 -0300

    ✨ Create action to export screenshots

---
+++ src/scripts/generate-gallery.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+const mediaPath = './src/media';
+const outputPath = './src/data/gallery.json';
+
+function formatName(fileName) {
+    return fileName
+        .replace(/\.[^/.]+$/, '')
+        .split('-')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+}
+
+const files = fs.readdirSync(mediaPath);
+
+const images = files
+    .filter(file => /\.(png|jpg|jpeg|webp)$/i.test(file))
+    .map(file => ({
+        file,
+        title: formatName(file)
+    }));
+
+fs.writeFileSync(outputPath, JSON.stringify(images, null, 2));
+
+console.log('Gallery generated:', images.length);

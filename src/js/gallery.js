1016ae48fc4e5c30c0cc82b72f7f73efd050be73
Roberto Almeida <almeida.robertop@hotmail.com>
2026-03-26 21:16:10 -0300

    ✨ Add swipe abillity

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,8 +1,48 @@
 const gallery = document.getElementById('gallery');
 const lightbox = document.getElementById('lightbox');
 const lightboxImg = document.getElementById('lightbox-img');
+let currentIndex = 0;
+let imagesList = [];
 
-function openImg(src) {
+let touchStartX = 0;
+let touchEndX = 0;
+
+function showNext() {
+  currentIndex = (currentIndex + 1) % imagesList.length;
+  updateImage();
+}
+
+function showPrev() {
+  currentIndex = (currentIndex - 1 + imagesList.length) % imagesList.length;
+  updateImage();
+}
+
+function updateImage() {
+  lightboxImg.style.opacity = 0;
+
+  setTimeout(() => {
+    lightboxImg.src = `src/screenshots/${imagesList[currentIndex].file}`;
+    lightboxImg.style.opacity = 1;
+  }, 150);
+}
+
+function handleSwipe() {
+  const diff = touchStartX - touchEndX;
+
+  if (Math.abs(diff) < 50) return;
+
+  if (diff > 0) {
+    showNext();
+  } else {
+    showPrev();
+  }
+}
+
+function openImg(index) {
+  currentIndex = index;
+
+  const src = `src/screenshots/${imagesList[index].file}`;
+
   lightboxImg.src = src;
   lightbox.style.display = 'flex';
 
@@ -31,6 +71,7 @@ fetch('src/data/gallery.json')
   .then(res => res.json())
   .then(images => {
 
+    imagesList = images;
     gallery.innerHTML = '';
 
     images.forEach((imgData, index) => {
@@ -41,7 +82,7 @@ fetch('src/data/gallery.json')
       img.title = imgData.title;
       img.loading = "lazy";
 
-      img.onclick = () => openImg(img.src);
+      img.onclick = () => openImg(index);
 
       gallery.appendChild(img);
 
@@ -55,4 +96,22 @@ fetch('src/data/gallery.json')
   })
   .catch(err => {
     console.error('Erro ao carregar screenshots:', err);
-  });
+  }
+);
+
+lightbox.addEventListener('touchstart', (e) => {
+  touchStartX = e.changedTouches[0].screenX;
+});
+
+lightbox.addEventListener('touchend', (e) => {
+  touchEndX = e.changedTouches[0].screenX;
+
+  handleSwipe();
+});
+
+document.addEventListener('keydown', (e) => {
+  if (lightbox.style.display !== 'flex') return;
+
+  if (e.key === 'ArrowRight') showNext();
+  if (e.key === 'ArrowLeft') showPrev();
+});
